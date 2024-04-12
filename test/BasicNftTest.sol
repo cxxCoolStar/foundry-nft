363f5570eac9a29bf58af840a61f9e901d996ad2
asta <1848505943@qq.com>
2024-04-12 19:41:41 +0800

    新增部署代码

--- test/BasicNftTest.sol
+++ test/BasicNftTest.sol
@@ -2,18 +2,34 @@
 pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
-import {DeployBasicNft} from "../script/DeployBasicNft.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
-    function setUp()  public {
+    function setUp() public {
         deployer = new DeployBasicNft();
         basicNft = deployer.run();
     }
 
     function testNameIsCorrect() public {
+        string memory expectName = "Dogie";
+        string memory actualName = basicNft.name();
+
+        assert(
+            keccak256(abi.encodePacked(expectName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG_URI);
+        assert(basicNft.balanceOf(USER) == 1);
     }
 }
